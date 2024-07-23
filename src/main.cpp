9b1220214ecb39e48e51a114a90fe047bd6e0a79
Parafernalha <douglas.oliveiras2210@gmail.com>
2024-07-22 21:03:03 -0300

    Modbus Server-Client communication throught transfering the BME680 temperature reading between each other

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+
+HardwareSerial SerialPort(2);
+
+const int Enable = 2;
+const int SlaveID = 1;
+String Temp = "Temperatura";
+
+void setup() 
+{
+  Serial.begin(115200);
+  SerialPort.begin(115200, SERIAL_8N1, 16, 17);
+  pinMode(Enable, OUTPUT);
+  digitalWrite(Enable, LOW);
+}
+
+void loop() 
+{
+  delay(500);
+
+  // Enable transmission mode
+  digitalWrite(Enable, HIGH);
+
+  // Send a test message to the slave
+ 
+  SerialPort.print(SlaveID);
+  //SerialPort.print(" ");
+  SerialPort.print(Temp);
+  //SerialPort.print("\n");
+  SerialPort.flush();
+
+  // Switch to receiving mode
+  digitalWrite(Enable, LOW);
+
+  // Wait for a response with a timeout
+  long startTime = millis();
+  while (!SerialPort.available()) {
+    if (millis() - startTime > 2000) { // 2-second timeout
+      Serial.println("Timeout waiting for response");
+      return;
+    }
+  }
+
+  // Read and print the response from the slave
+  if (SerialPort.available()) {
+    String response = SerialPort.readString();
+    Serial.println(response);
+  }
+  delay(1000);
+}
