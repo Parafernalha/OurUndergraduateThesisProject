094fd32f3ac985639d66624b88758b39674d7f9b
Parafernalha <douglas.oliveiras2210@gmail.com>
2024-07-24 22:18:50 -0300

    Client V01 -  implementation of JSON

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
+#include <ArduinoJson.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
@@ -14,17 +15,31 @@ const int Enable = 2;
 const int SlaveNumber = 1;
 int Slave;
 
+void sendSensorData(const char* key, float value) {
+  StaticJsonDocument<200> doc;
+  doc[key] = value;
+  String json;
+  serializeJson(doc, json);
+
+  digitalWrite(Enable, HIGH);
+  SerialPort.print(json);
+  SerialPort.flush();
+  digitalWrite(Enable, LOW);
+}
+
 void setup() 
 {
   Serial.begin(115200);
   SerialPort.begin(115200, SERIAL_8N1, 16, 17);
   pinMode(Enable, OUTPUT);
   digitalWrite(Enable, LOW);
-    if (!bme.begin()) 
+
+  if (!bme.begin()) 
   {
     Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
     while (1);
   }
+
   // Set up oversampling and filter initialization
   bme.setTemperatureOversampling(BME680_OS_8X);
   bme.setHumidityOversampling(BME680_OS_2X);
@@ -32,115 +47,74 @@ void setup()
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); // 320*C for 150 ms
 }
+
 void loop() 
 {
   digitalWrite(Enable, LOW);
-  if (SerialPort.available()){
+  if (SerialPort.available()) {
     Slave = SerialPort.parseInt();
-       if(Slave == SlaveNumber){
-            String receivedMessage = SerialPort.readString();  
-            Serial.println(receivedMessage);
-              if(receivedMessage == "Temperatura"){
-                  // Tell BME680 to begin measurement.
-                  bme.beginReading();
-                  delay(100);
-                  if (!bme.performReading()){
-                    Serial.println("Failed to perform reading :(");
-                    return;}
-                  float temperature = bme.temperature;
-                  Serial.println(temperature);
-                  // Enable transmission mode
-                  digitalWrite(Enable, HIGH);
-                  SerialPort.print("Temperature = ");
-                  SerialPort.print(temperature);
-                  SerialPort.print(" ºC");
-                  SerialPort.flush();
-                  // Switch to receiving mode
-                  digitalWrite(Enable, LOW);}
-                
-                  else if(receivedMessage == "Humidade"){
-                    // Tell BME680 to begin measurement.
-                  bme.beginReading();
-                  delay(100);
-                  if (!bme.performReading()){
-                    Serial.println("Failed to perform reading :(");
-                    return;}
-                  float humidity = bme.humidity;
-                  Serial.println(humidity);
-                  // Enable transmission mode
-                  digitalWrite(Enable, HIGH);
-                  SerialPort.print("Humidity =  ");
-                  SerialPort.print(humidity);
-                  SerialPort.print(" %");
-                  SerialPort.flush();
-                  // Switch to receiving mode
-                  digitalWrite(Enable, LOW);}
+    if (Slave == SlaveNumber) {
+      String receivedMessage = SerialPort.readString();
+      receivedMessage.trim();
+      Serial.println(receivedMessage);
 
-                   else if(receivedMessage == "Gases"){
-                    // Tell BME680 to begin measurement.
-                  bme.beginReading();
-                  delay(100);
-                  if (!bme.performReading()){
-                    Serial.println("Failed to perform reading :(");
-                    return;}
-                  float Gas = bme.gas_resistance;
-                  Serial.println(Gas);
-                  // Enable transmission mode
-                  digitalWrite(Enable, HIGH);
-                  SerialPort.print("Gas = ");
-                  SerialPort.print(Gas);
-                  SerialPort.print(" KOhms");
-                  SerialPort.flush();
-                  // Switch to receiving mode
-                  digitalWrite(Enable, LOW);}
-
-                  else if(receivedMessage == "Pressao"){
-                    // Tell BME680 to begin measurement.
-                  bme.beginReading();
-                  delay(100);
-                  if (!bme.performReading()){
-                    Serial.println("Failed to perform reading :(");
-                    return;}
-                  float Pre = bme.pressure;
-                  Serial.println(Pre);
-                  // Enable transmission mode
-                  digitalWrite(Enable, HIGH);
-                  SerialPort.print("Pressure: ");
-                  SerialPort.print(Pre);
-                  SerialPort.print(" hPa");
-                  SerialPort.flush();
-                  // Switch to receiving mode
-                  digitalWrite(Enable, LOW);}
-
-                  else if(receivedMessage = "TemperaturaHumidadeGasesPressao"){
-                  // Tell BME680 to begin measurement.
-                  bme.beginReading();
-                  delay(100);
-                  if (!bme.performReading()){
-                    Serial.println("Failed to perform reading :(");
-                    return;}
-                  float temperature = bme.temperature;
-                  float humidity = bme.humidity;
-                  float Gas = bme.gas_resistance;
-                  float Pre = bme.pressure;
-                  // Enable transmission mode
-                  digitalWrite(Enable, HIGH);
-                  SerialPort.print("Temperature = ");
-                  SerialPort.print(temperature);
-                  SerialPort.print(" ºC");
-                  SerialPort.print("Humidity =  ");
-                  SerialPort.print(humidity);
-                  SerialPort.print(" %");
-                  SerialPort.print("Pressure: ");
-                  SerialPort.print(Pre);
-                  SerialPort.print(" hPa");
-                  SerialPort.print("Gas = ");
-                  SerialPort.print(Gas);
-                  SerialPort.print(" KOhms");
-                  SerialPort.flush();
-                  // Switch to receiving mode
-                  digitalWrite(Enable, LOW);
+      if (receivedMessage == "Temperatura") {
+        bme.beginReading();
+        delay(100);
+        if (!bme.performReading()) {
+          Serial.println("Failed to perform reading :(");
+          return;
         }
-      }             
-   } 
+        sendSensorData("Temperature", bme.temperature);
+      } 
+      else if (receivedMessage == "Humidade") {
+        bme.beginReading();
+        delay(100);
+        if (!bme.performReading()) {
+          Serial.println("Failed to perform reading :(");
+          return;
+        }
+        sendSensorData("Humidity", bme.humidity);
+      } 
+      else if (receivedMessage == "Gases") {
+        bme.beginReading();
+        delay(100);
+        if (!bme.performReading()) {
+          Serial.println("Failed to perform reading :(");
+          return;
+        }
+        sendSensorData("Gas", bme.gas_resistance);
+      } 
+      else if (receivedMessage == "Pressao") {
+        bme.beginReading();
+        delay(100);
+        if (!bme.performReading()) {
+          Serial.println("Failed to perform reading :(");
+          return;
+        }
+        sendSensorData("Pressure", bme.pressure);
+      } 
+      else if (receivedMessage == "TemperaturaHumidadeGasesPressao") {
+        bme.beginReading();
+        delay(100);
+        if (!bme.performReading()) {
+          Serial.println("Failed to perform reading :(");
+          return;
+        }
+
+        StaticJsonDocument<200> doc;
+        doc["Temperature"] = bme.temperature;
+        doc["Humidity"] = bme.humidity;
+        doc["Gas"] = bme.gas_resistance;
+        doc["Pressure"] = bme.pressure;
+        String json;
+        serializeJson(doc, json);
+
+        digitalWrite(Enable, HIGH);
+        SerialPort.print(json);
+        SerialPort.flush();
+        digitalWrite(Enable, LOW);
+      }
+    }
+  }
 }
